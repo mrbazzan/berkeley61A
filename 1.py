5119219bab221442f4d93f8657a3841188be2d60
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2023-09-29 20:09:19 +0100

    pro: berkeley: hw1 cont.

--- 1.py
+++ 1.py
@@ -42,3 +42,45 @@ def sum_of_squares_of_two_larger_numbers(a, b, c):
     18
     """
     return pow(max(a,b,c), 2) + pow(min(max(a,b),max(a,c),max(b,c)), 2)
+
+# 3) Show that `with_if_function` doesn't work the same way as
+#    `with_if_statement`
+
+def if_function(condition, true_result, false_result):
+    """Return true_result if condition is a true value,
+       and false_result otherwise.
+    """
+
+    if condition:
+        return true_result
+    else:
+        return false_result
+
+
+def with_if_statement():
+    if c():
+        return t()
+    else:
+        return f()
+
+def with_if_function():
+    return if_function(c(), t(), f())
+
+num = 0
+
+def c():
+    return False
+
+def t():
+    global num
+    num = 1
+
+def f():
+    return num
+
+# 
+print(with_if_statement())
+
+# All expressions are evaluated before passed to the formal parameter
+print(with_if_function())
+
