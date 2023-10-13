cc3d23242d13a81e3e449e660ce531057d697d42
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2023-10-13 17:14:13 +0100

    pingpong iterative solution

--- recursion.py
+++ recursion.py
@@ -34,3 +34,45 @@ def num_sevens(n):
         return 1 + num_sevens(n // 10)
     else:
         return num_sevens(n // 10)
+
+from operator import add, sub
+
+def pingpong(n):
+    """Return the nth element of the ping-pong sequence.
+
+    >>> pingpong(7)
+    7
+    >>> pingpong(8)
+    6
+    >>> pingpong(15)
+    1
+    >>> pingpong(21)
+    -1
+    >>> pingpong(22)
+    0
+    >>> pingpong(30)
+    6
+    >>> pingpong(68)
+    2
+    >>> pingpong(69)
+    1
+    >>> pingpong(70)
+    0
+    >>> pingpong(71)
+    1
+    >>> pingpong(72)
+    0
+    >>> pingpong(100)
+    2
+    """
+    op = add
+    count, index = 0, 0
+    while index < n:
+        count = op(count, 1)
+
+        if ((index+1) % 7 == 0) or num_sevens(index+1) > 0:
+            op = sub if op is add else add
+
+        index += 1
+
+    return count
