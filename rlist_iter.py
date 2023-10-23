aa04374848836fd2a708ab0e341af2e5496ce251
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2023-10-23 07:02:48 +0100

    sequence abstraction: rlist

---
+++ rlist_iter.py
@@ -0,0 +1,28 @@
+
+empty_rlist = None
+
+def make_rlist(first, rest):
+    return first, rest
+
+def first(s):
+    return s[0]
+
+def rest(s):
+    return s[1]
+
+def get_length(rlist):
+    count = 0
+    while rlist != empty_rlist:
+        rlist = rest(rlist)
+        count += 1
+    return count
+
+def get_item(rlist, index):
+    assert index < get_length(rlist), "rlist out of range"
+    while index > 0:
+        rlist = rest(rlist)
+        index -= 1
+    return first(rlist)
+
+rlist = make_rlist(1, make_rlist(3, make_rlist(5, make_rlist(4, empty_rlist))))
+print(get_item(rlist, 5))
