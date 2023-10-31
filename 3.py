58e85b38dac83875d109a92ce4a49ec9272f27bb
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2023-10-31 21:44:06 +0100

    3.py: mobiles

--- 3.py
+++ 3.py
@@ -102,3 +102,60 @@ def replace_leaf(t, old, new):
         lst += [replace_leaf(branch, old, new)]
 
     return tree(label(t), lst)
+
+"""
+Mobiles
+"""
+
+def mobile(left, right):
+    """Construct a mobile from a left side and a right side."""
+    assert is_side(left), "left must be a side"
+    assert is_side(right), "right must be a side"
+    return ['mobile', left, right]
+
+def is_mobile(m):
+    """Return whether m is a mobile."""
+    return type(m) == list and len(m) == 3 and m[0] == 'mobile'
+
+def left(m):
+    """Select the left side of a mobile."""
+    assert is_mobile(m), "must call left on a mobile"
+    return m[1]
+
+def right(m):
+    """Select the right side of a mobile."""
+    assert is_mobile(m), "must call right on a mobile"
+    return m[2]
+
+def side(length, mobile_or_weight):
+    """Construct a side: a length of rod with a mobile or weight at the end."""
+    assert is_mobile(mobile_or_weight) or is_weight(mobile_or_weight)
+    return ['side', length, mobile_or_weight]
+
+def is_side(s):
+    """Return whether s is a side."""
+    return type(s) == list and len(s) == 3 and s[0] == 'side'
+
+def length(s):
+    """Select the length of a side."""
+    assert is_side(s), "must call length on a side"
+    return s[1]
+
+def end(s):
+    """Select the mobile or weight hanging at the end of a side."""
+    assert is_side(s), "must call end on a side"
+    return s[2]
+
+def weight(size):
+    """Construct a weight of some size."""
+    assert size > 0
+    return ['weight', size]
+
+def size(w):
+    """Select the size of a weight."""
+    assert is_weight(w), 'must call size on a weight'
+    return w[1]
+
+def is_weight(w):
+    """Whether w is a weight."""
+    return type(w) == list and len(w) == 2 and w[0] == 'weight'
